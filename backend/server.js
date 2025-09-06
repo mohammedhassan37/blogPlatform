60aae9b93a756b36221791de7e3288ac2be30eb1
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-09-06 08:20:16 +0100

    Set up basic Express server and add frontend index.html
    Initialized a basic Express server with PostgreSQL connection using environment variables. Added a simple frontend index.html file as a placeholder.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import {Pool} from 'pg';
+ 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+})
 
-app.listen(process.env.PORT);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,() =>{
+    console.log("Port running on " + PORT)
+});
